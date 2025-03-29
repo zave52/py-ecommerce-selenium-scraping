51060b0879b6e5a0f0503e77a91b325d926a715e
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-03-29 15:32:12 +0200

    create parse_single_product function

--- app/parse.py
+++ app/parse.py
@@ -2,6 +2,9 @@ import csv
 from dataclasses import dataclass, fields, astuple
 from urllib.parse import urljoin
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
 BASE_URL = "https://webscraper.io/"
 HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
 
@@ -16,6 +19,24 @@ class Product:
 
 
 PRODUCT_FIELDS = [field.name for field in fields(Product)]
+
+
+def parse_single_product(product: WebElement) -> Product:
+    return Product(
+        title=product.find_element(By.CLASS_NAME, "title").text,
+        description=product.find_element(By.CLASS_NAME, "description").text,
+        price=float(
+            product.find_element(By.CLASS_NAME, "price")
+            .text.replace("$", "")
+        ),
+        rating=int(
+            product.find_element(By.CLASS_NAME, "review-count")
+            .text.split()[0]
+        ),
+        num_of_reviews=len(
+            product.find_elements(By.CLASS_NAME, "ws-icon-star")
+        )
+    )
 
 
 def write_products_to_csv(csv_path: str, products: list[Product]) -> None:
